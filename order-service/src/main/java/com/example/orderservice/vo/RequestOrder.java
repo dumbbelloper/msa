051653e4cc58b39ccd265ce84a8d7cf798b52584
dumbbelloper <dumbbelloper@gmail.com>
2023-01-12 20:42:44 +0900

    order-service 추가

---
+++ order-service/src/main/java/com/example/orderservice/vo/RequestOrder.java
@@ -0,0 +1,13 @@
+package com.example.orderservice.vo;
+
+
+import lombok.Data;
+
+@Data
+public class RequestOrder {
+    private String productId;
+
+    private String qty;
+
+    private String unitPrice;
+}
